cee1dd1935bdbb15c8250a44691bd4b66e5a5986
Yann Klein <yann.klein@me.com>
2024-03-29 12:02:43 +0900

    create basic expressjs app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to OpenAI API proxy server. X calls remaining today!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});

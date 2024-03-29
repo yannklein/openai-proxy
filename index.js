6a8333b1b7f34711b5218acf8109405632af6002
Yann Klein <yann.klein@me.com>
2024-03-29 15:08:01 +0900

    move mongo client instance creation into each route

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   try {
-    const apiCallsCounter = new ApiCallsCounter();
-    const amountCalls = await apiCallsCounter.displayCalls();
-    res.send(`Welcome to OpenAI API proxy server. ${apiCallsCounter.callLimit - amountCalls} calls remaining today!`);
-    
+    // const apiCallsCounter = new ApiCallsCounter();
+    // const amountCalls = await apiCallsCounter.displayCalls();
+    // res.send(`Welcome to OpenAI API proxy server. ${apiCallsCounter.callLimit - amountCalls} calls remaining today!`);
+    res.send("hi")
   } catch (error) {
     res.send("Some error occured: " + error)
   }

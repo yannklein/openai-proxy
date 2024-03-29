935bc929fb9817b635834e45d4dc9d8322716457
Yann Klein <yann.klein@me.com>
2024-03-29 14:36:47 +0900

    add call limit to post api

--- api-calls-counter.js
+++ api-calls-counter.js
@@ -1,24 +1,55 @@
 import { MongoClient } from "mongodb";
 
 export default class ApiCallsCounter {
+  #callLimit = 10
+  
   constructor() {
     const client = new MongoClient(process.env.MONGODB_URI);
     const database = client.db('openai-proxy');
     this.stats = database.collection('stats');
   }
 
+  get callLimit() {
+    return this.#callLimit;
+  }
+
   async displayCalls() {
     let stat = await this.stats.findOne();
-    if (stat.current_day !== new Date()) {
+    // reset the call available if new day
+    if (this.isNotToday(stat.current_day)) {
       stat = await this.resetCalls();
-      console.log(stat);
     }
     return stat.calls;
   }
 
-  async resetCalls() {
-    const result = await this.stats.updateOne({}, {$set: {calls: 0, current_day: new Date()}})
+  async autorizeCall() {
+    let stat = await this.stats.findOne();
+    // reset the call available if new day
+    if (this.isNotToday(stat.current_day)) {
+      stat = await this.resetCalls();
+    }
+    // unautorize if no more call available
+    if (stat.calls >= this.#callLimit) {
+      return false;
+    }
+    // remove one call as at least one is available
+    const result = await this.stats.updateOne({}, {$set: {calls: stat.calls + 1}})
     console.log(result);
+    return true;
+  }
+
+  async resetCalls() {
+    await this.stats.updateOne({}, {$set: {calls: 0, current_day: this.todayMidnight()}})
     return await this.stats.findOne();
+  }
+
+  todayMidnight() {
+    const today = new Date();
+    return new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+  }  
+
+  isNotToday(rawDate) {
+    const someDay = new Date(rawDate);
+    return someDay.getTime() !== this.todayMidnight().getTime();
   }
 }
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const apiCallsCounter = new ApiCallsCounter();
 app.get('/', async (req, res) => {
   try {
     const amountCalls = await apiCallsCounter.displayCalls();
-    res.send(`Welcome to OpenAI API proxy server. ${10 - amountCalls} calls remaining today!`);
+    res.send(`Welcome to OpenAI API proxy server. ${apiCallsCounter.callLimit - amountCalls} calls remaining today!`);
     
   } catch (error) {
     res.send("Some error occured: " + error)
@@ -20,10 +20,14 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-  const { messages, format } = req.body;
-  const openaiApi = new OpenaiApiClient();
-  const completion = await openaiApi.call(messages, format);
-  res.send(completion);
+  if ( await apiCallsCounter.autorizeCall()) {
+    const { messages, format } = req.body;
+    const openaiApi = new OpenaiApiClient();
+    const completion = await openaiApi.call(messages, format);
+    res.send(completion);
+  } else {
+    res.send('{"error": "No more call token available for today"}');
+  }
 });
 
 app.listen(3000, () => {
